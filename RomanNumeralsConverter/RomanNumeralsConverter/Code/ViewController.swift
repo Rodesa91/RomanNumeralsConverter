8e93df01b0a046f6ab5750d790b5021abec802a5
Rodrigo de Santiago <r.desantiago@globant.com>
2018-06-10 14:26:42 -0300

    Create Project and Structure
    Create new project and define the project structure

---
+++ RomanNumeralsConverter/RomanNumeralsConverter/Code/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RomanNumeralsConverter
+//
+//  Created by Rodrigo de Santiago on 10/6/18.
+//  Copyright © 2018 Rodrigo de Santiago. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
